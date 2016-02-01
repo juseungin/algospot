9f93f80fa276b4a49949adb2de7aa36ecc70eed5
juseungjin <juseungjin@gmail.com>
2016-02-01 16:01:28 +0900

    Update FIXPAREN.py

--- FIXPAREN.py
+++ FIXPAREN.py
@@ -1,19 +1,13 @@
 import sys
 
-rl = lambda : sys.stdin.readline()
-
-closer = {'}':'{', ')':'(', '>':'<', ']':'['}
 opener = {'{':'}', '(':')', '<':'>', '[':']'}
+closer = {'}':'{', ')':'(', '>':'<', ']':'['}
 order = {'{':0, '(':1, '<':2, '[':3 }
 
-def main():
-    repeat = int(rl())
-    output = []
-    for n in range(repeat):
-        s, priority = rl().split()
-    
-        for i in range(4):
-            order[priority[i]] = 4-i
+def fix_paren(s, priority):
+    global opner,closer, order
+    for i in range(4):
+        order[priority[i]] = 4-i
         
         stack = []
         ans = []
@@ -43,16 +37,21 @@ def main():
                     
             else :
                 stack.append(parentheses)
-                pushed = True         
-        output.append(''.join(ans))
+                pushed = True
+    return ''.join(ans)      
+
+def main():
+    rl = lambda : sys.stdin.readline()
+    
+    repeat = int(rl())
+    output = []
+    for n in range(repeat):
+        s, priority = rl().split()    
+        
+        output.append(fix_paren(s,priority))
     
     for ans in output:
         print(ans)
     
 if __name__ == '__main__':
     main()
-
-# 3
-# (} {(<[
-# ()([)> <({[
-# <{(][>(}>] <[{(
