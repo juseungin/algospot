15ef319ca8ed6bdfa96a5b62115c538f3f6a0f82
juseungjin <juseungjin@gmail.com>
2016-02-04 23:43:26 +0900

    Update NQUEEN.py

--- NQUEEN.py
+++ NQUEEN.py
@@ -1,17 +1,14 @@
 import sys
-# for ol in permutations('ABC'):
-#     print(ol)
 
 rl = lambda : sys.stdin.readline()
 
 def valid(input):
-    for i in range(len(input)):
-        for j in range(len(input)):
-            if i == j : continue
-            if input[i] - input[j] == i - j :
+    for x in range(len(input)):
+        for y in range(len(input)):
+            if x == y : continue
+            if abs(input[x] - input[y]) == abs(x - y) :
                 return False
     return True 
-
 
 def perm (elements):
     if len(elements) == 1:
@@ -19,12 +16,31 @@ def perm (elements):
     else:
         for p in perm(elements[1:]):
             for i in range(len(elements)):
-                yield p[:i] elements[0],p[i:]
+                yield p[:i] + elements[0] + p[i:]
 
+def str_to_hex(string):
+    li = []
+    for ch in string:
+        li.append(int(ch,16))
+    return li
 
-N = '123456789ABC'
+def hex_to_str(li):
+    out_str= ''
+    for num in li:
+        if num == 10 : num = 'A'
+        elif num == 11 : num = 'B'
+        elif num == 12 : num = 'C'
+        elif num == 13 : num = 'D'
+        elif num == 14 : num = 'E'
+        elif num == 15 : num = 'F'
+        out_str += str(num)
+    return out_str
 
-input = '123'
-
-for ol in perm([1,2,3]):
-    print(ol)
+def main():
+    input = [1,2,3,4,5,6,7,8,9,10]
+    cnt = 0
+    for s in perm(hex_to_str(input)):
+        if valid(str_to_hex(s)) : cnt +=1
+    print(cnt)
+if __name__ == '__main__':
+    main()
